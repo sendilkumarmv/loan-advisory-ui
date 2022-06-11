8305f92099028e97ae1900d699813eb2593d0177
Sendilkumar Murugaiah <sendilkumarmurugaiah@gmail.com>
2022-06-11 21:07:56 +0530

    Few more components. WIP.

--- web-ui/src/app/app-routing.module.ts
+++ web-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActionsComponent } from './actions/actions.component';
+import { ReqAdvancedComponent } from './req-advanced/req-advanced.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ActionsComponent},
+  { path: 'reqad', component: ReqAdvancedComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReqAdvancedComponent } from './req-advanced/req-advanced.component';
+import { ActionsComponent } from './actions/actions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReqAdvancedComponent,
+    ActionsComponent
   ],
   imports: [
     BrowserModule,
